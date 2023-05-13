c362dfa04ec663caa55de0352c82c50b94cc4519
Adrien <adrien.dalbeigue@cpe.fr>
2023-05-13 17:50:23 +0200

    k

--- Karmine.py
+++ Karmine.py
@@ -5,8 +5,10 @@ CLE_API = 'RGAPI-e36f5233-1d2a-4bf2-b91e-9cfe410de395'
 
 def envoie_demande_liste_challengers(api_key):
     """envoie une demande GET à l'API Riot pour obtenir la liste des ID des joueurs challengers"""
+    requete = f'https://euw1.api.riotgames.com/lol/league/v4/challengerleagues/by-queue/RANKED_SOLO_5x5?api_key={api_key}'
+    print(requete)
     # Exemple de requête GET
-    response = requests.get('https://euw1.api.riotgames.com/lol/league/v4/challengerleagues/by-queue/RANKED_SOLO_5x5?api_key={api_key}')
+    response = requests.get(requete)
 
     # Vérifier le code de réponse
     if response.status_code == 200:
