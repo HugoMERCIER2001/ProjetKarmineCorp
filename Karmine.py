d7cc1235d3d6a232c2c698be4e0c357523f1120c
Adrien <adrien.dalbeigue@cpe.fr>
2023-05-13 17:08:06 +0200

    requete http

--- Karmine.py
+++ Karmine.py
@@ -1,1 +1,9 @@
 import requests
+
+
+
+def requestAPI(api_key):
+    response = requests.get("https://euw1.api.riotgames.com/lol/league/v4/challengerleagues/by-queue/RANKED_SOLO_5x5?api_key{api_key}")
+    return response.json()
+
+
