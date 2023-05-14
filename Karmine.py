b74449b2ed7089381fd429aa88931dd98a0c779c
Your Name~requests <hugo.mercier1@grenoble-inp.org>
2023-05-14 18:14:58 +0200

    nouveau push

--- Karmine.py
+++ Karmine.py
@@ -16,13 +16,13 @@ def envoie_demande_liste_challengers(api_key):
     # Vérifier le code de réponse
     if response.status_code == 200:
     # La requête a réussi
-    data = response.json() # Récupérer les données de réponse au format JSON
-    # Traiter les données
-    with open("data_challenger_id.json", "w") as f:
-    json.dump(data, f)
+        data = response.json() # Récupérer les données de réponse au format JSON
+        # Traiter les données
+        with open("data_challenger_id.json", "w") as f:
+            json.dump(data, f)
     # Gérer les erreurs de requête
     else:
-    print('La requête a échoué. Code de réponse :', response.status_code)
+        print('La requête a échoué. Code de réponse :', response.status_code)
 
 def envoie_demande_PUUID_summoner(summoner_id, api_key):
     """envoie une demande GET à l'API Riot pour obtenir le PUUID d'un joueur de summonerID : 'summoner_id'"""
@@ -32,14 +32,14 @@ def envoie_demande_PUUID_summoner(summoner_id, api_key):
 
     # Vérifier le code de réponse
     if response.status_code == 200:
-    # La requête a réussi
-    data = response.json() # Récupérer les données de réponse au format JSON
-    # Traiter les données
-    with open("data_summoner_id.json", "w") as f:
-    json.dump(data, f)
+        # La requête a réussi
+        data = response.json() # Récupérer les données de réponse au format JSON
+        # Traiter les données
+        with open("data_summoner_id.json", "w") as f:
+            json.dump(data, f)
     # Gérer les erreurs de requête
     else:
-    print('La requête a échoué. Code de réponse :', response.status_code)
+        print('La requête a échoué. Code de réponse :', response.status_code)
 
 def envoie_demande_liste_match_challengers(api_key):
     """envoie une demande GET à l'API Riot pour obtenir la liste des ID des matchs niveau Challengers"""
@@ -49,14 +49,14 @@ def envoie_demande_liste_match_challengers(api_key):
 
     # Vérifier le code de réponse
     if response.status_code == 200:
-    # La requête a réussi
-    data = response.json() # Récupérer les données de réponse au format JSON
-    # Traiter les données
-    with open("data_match_idea.json", "w") as f:
-    json.dump(data, f)
+        # La requête a réussi
+        data = response.json() # Récupérer les données de réponse au format JSON
+        # Traiter les données
+        with open("data_match_idea.json", "w") as f:
+            json.dump(data, f)
     # Gérer les erreurs de requête
     else:
-    print('La requête a échoué. Code de réponse :', response.status_code)
+        print('La requête a échoué. Code de réponse :', response.status_code)
 ##############################################Partie PostgreSQL######################################################################
 
 conn = psycopg2.connect( #Instaure une connexion vers la database DigitalOcean.
