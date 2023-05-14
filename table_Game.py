b8946ae9331a62d321e01371191ce1f382081bb3
Adrien <adrien.dalbeigue@cpe.fr>
2023-05-14 21:23:56 +0200

    table game et liaison

--- table_Game.py
+++ table_Game.py
@@ -4,5 +4,12 @@ import json
 import time
 from API_Riots import *
 
+def create_tables(cursor,conn):
+    cursor.execute("CREATE TABLE matches (matchId VARCHAR(50) PRIMARY KEY, file JSON);")
+    conn.commit()
+
+def rempli_table_match(cursor,conn,matchId,file):
+    cursor.execute("INSERT INTO matches (matchId,file) VALUES (%s,%s);",(matchId,file))
+    conn.commit()
 
 
--- table_Liaison.py
+++ table_Liaison.py
@@ -4,3 +4,10 @@ import json
 import time
 from API_Riots import *
 
+def create_table(cursor,conn):
+    cursor.execute("CREATE TABLE Liaison (matchId VARCHAR(50), summonerId VARCHAR(50), PRIMARY KEY (matchId,summonerId));")
+    conn.commit()
+
+def rempli_table_liaison(matchId,summonerId,cursor,conn):
+    cursor.execute("INSERT INTO Liaison (matchId,summonerId) VALUES (%s,%s);",(matchId,summonerId))
+    conn.commit()
